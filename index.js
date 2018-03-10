5a309a8ade2c602fa5c6a2c4045fe77ed4ec92bd
Roman Vinokurov <r.vinokurov@yclients.tech>
2018-03-10 22:57:58 +0300

    rewrite preprocess loader to typescript

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
 "use strict";
+var path = require("path");
+var process = require("process");
+var pp = require("preprocess");
+var loaderUtils = require("loader-utils");
+var lodash_1 = require("lodash");
 var loader = function (content) {
     this.cachable && this.cachable();
+    var resourcePath = this.resourcePath;
     console.log(this.resourcePath);
-    return content;
+    if (typeof resourcePath !== 'string' || !resourcePath.length) {
+        this.callback(null, content);
+        return;
+    }
+    var extension = path.extname(this.resourcePath).substring(1);
+    var options = {
+        srcDir: this.context,
+        type: extension
+    }, query = loaderUtils.getOptions(this) || {};
+    if (lodash_1.isObject(query.ppOptions)) {
+        options = lodash_1.merge(options, query.ppOptions);
+        delete query.ppOptions;
+    }
+    var context = lodash_1.merge({}, process.env, query);
+    context.NODE_ENV = context.NODE_ENV || 'development';
+    var processed = pp.preprocess(content, context, options);
+    this.callback(null, processed);
 };
 module.exports = loader;
--- index.ts
+++ index.ts
@@ -1,7 +1,41 @@
+import * as path from 'path';
+import * as process from 'process';
+import * as pp from 'preprocess';
+import * as loaderUtils from 'loader-utils';
+import {isObject, merge} from 'lodash';
+
+
 const loader = function (content) {
     this.cachable && this.cachable();
+
+    let {resourcePath} = this;
+
     console.log(this.resourcePath);
-    return content;
+
+    if(typeof resourcePath !== 'string' || !resourcePath.length) {
+        this.callback(null,  content);
+        return;
+    }
+
+    const
+        extension = path.extname(this.resourcePath).substring(1);
+
+    let options = {
+            srcDir :this.context,
+            type : extension
+        },
+        query = loaderUtils.getOptions(this) || {};
+
+    if(isObject(query.ppOptions)) {
+        options = merge(options, query.ppOptions);
+        delete query.ppOptions;
+    }
+    const context = merge({}, process.env, query);
+    context.NODE_ENV = context.NODE_ENV || 'development';
+
+    let processed = pp.preprocess(content, context, options);
+
+    this.callback(null, processed);
 };
 
 export = loader;
