f2672dd98299834dbf9a63b4a450617df85e99c9
Roman Vinokurov <r.vinokurov@yclients.tech>
2018-03-11 18:08:26 +0300

    add log processed files

--- index.js
+++ index.js
@@ -1,18 +1,35 @@
 "use strict";
 var path = require("path");
+var fs = require("fs");
 var process = require("process");
+var crypto = require("crypto");
 var pp = require("preprocess");
 var loaderUtils = require("loader-utils");
 var lodash_1 = require("lodash");
+var algorithm = 'sha1', encoding = 'utf8', outputEncoding = 'hex', checksum = function (inputString) {
+    return crypto
+        .createHash(algorithm)
+        .update(inputString, encoding)
+        .digest(outputEncoding);
+}, getTouchedFiles = function (ppLogFile) {
+    try {
+        return lodash_1.compact(fs.readFileSync(ppLogFile, encoding).split("\n"));
+    }
+    catch (e) {
+        return [];
+    }
+}, saveTouchedFiles = function (ppLogFile, touchedFiles) {
+    fs.writeFileSync(ppLogFile, touchedFiles.join("\n"));
+};
 var loader = function (content) {
-    this.cachable && this.cachable();
+    this.cacheable && this.cacheable();
     var resourcePath = this.resourcePath;
     console.log(this.resourcePath);
     if (typeof resourcePath !== 'string' || !resourcePath.length) {
         this.callback(null, content);
         return;
     }
-    var extension = path.extname(this.resourcePath).substring(1);
+    var extension = path.extname(this.resourcePath).substring(1), sourceChecksum = checksum(content), touchedFiles = [];
     var options = {
         srcDir: this.context,
         type: extension
@@ -21,9 +38,18 @@ var loader = function (content) {
         options = lodash_1.merge(options, query.ppOptions);
         delete query.ppOptions;
     }
+    if (typeof query.ppLogFile === 'string') {
+        touchedFiles.push.apply(touchedFiles, getTouchedFiles(query.ppLogFile));
+    }
     var context = lodash_1.merge({}, process.env, query);
     context.NODE_ENV = context.NODE_ENV || 'development';
     var processed = pp.preprocess(content, context, options);
+    var processedChecksum = checksum(processed);
+    console.log(sourceChecksum, processedChecksum);
+    if (typeof query.ppLogFile === 'string' && sourceChecksum !== processedChecksum) {
+        touchedFiles.push(resourcePath);
+        saveTouchedFiles(query.ppLogFile, lodash_1.uniq(touchedFiles));
+    }
     this.callback(null, processed);
 };
 module.exports = loader;
--- index.ts
+++ index.ts
@@ -1,41 +1,81 @@
 import * as path from 'path';
+import * as fs from 'fs';
 import * as process from 'process';
+import * as crypto from 'crypto';
 import * as pp from 'preprocess';
 import * as loaderUtils from 'loader-utils';
-import {isObject, merge} from 'lodash';
+import {isObject, merge, uniq, compact} from 'lodash';
+import * as webpack from 'webpack';
+import LoaderContext = webpack.loader.LoaderContext;
+import {OptionObject} from 'loader-utils';
 
+const
+  algorithm = 'sha1',
+  encoding = 'utf8',
+  outputEncoding = 'hex',
 
-const loader = function (content) {
-    this.cachable && this.cachable();
+  checksum = (inputString: string): string => {
+    return crypto
+      .createHash(algorithm)
+      .update(inputString, encoding)
+      .digest(outputEncoding);
+  },
 
-    let {resourcePath} = this;
-
-    console.log(this.resourcePath);
-
-    if(typeof resourcePath !== 'string' || !resourcePath.length) {
-        this.callback(null,  content);
-        return;
+  getTouchedFiles = (ppLogFile: string): string[] => {
+    try {
+      return compact(fs.readFileSync(ppLogFile, encoding).split("\n"));
+    } catch (e) {
+      return [];
     }
+  },
 
-    const
-        extension = path.extname(this.resourcePath).substring(1);
+  saveTouchedFiles = (ppLogFile: string, touchedFiles: string[]) : void =>  {
+    fs.writeFileSync(ppLogFile, touchedFiles.join("\n"));
+  };
 
-    let options = {
-            srcDir :this.context,
-            type : extension
-        },
-        query = loaderUtils.getOptions(this) || {};
+const loader = function (this: LoaderContext, content: string): void {
+  this.cacheable && this.cacheable();
 
-    if(isObject(query.ppOptions)) {
-        options = merge(options, query.ppOptions);
-        delete query.ppOptions;
-    }
-    const context = merge({}, process.env, query);
-    context.NODE_ENV = context.NODE_ENV || 'development';
+  let {resourcePath} = this;
 
-    let processed = pp.preprocess(content, context, options);
+  if (typeof resourcePath !== 'string' || !resourcePath.length) {
+    this.callback(null, content);
+    return;
+  }
 
-    this.callback(null, processed);
+  const
+    extension = path.extname(this.resourcePath).substring(1),
+    sourceChecksum = checksum(content),
+    touchedFiles = [];
+
+  let options = {
+      srcDir: this.context,
+      type: extension
+    },
+    query: OptionObject = loaderUtils.getOptions(this) || {};
+
+  if (isObject(query.ppOptions)) {
+    options = merge(options, query.ppOptions);
+    delete query.ppOptions;
+  }
+
+  if (typeof query.ppLogFile === 'string') {
+    touchedFiles.push(...getTouchedFiles(query.ppLogFile));
+  }
+
+  const context = merge({}, process.env, query);
+  context.NODE_ENV = context.NODE_ENV || 'development';
+
+  let processed: string = pp.preprocess(content, context, options);
+
+  const processedChecksum = checksum(processed);
+
+  if (typeof query.ppLogFile === 'string' && sourceChecksum !== processedChecksum) {
+    touchedFiles.push(resourcePath);
+    saveTouchedFiles(query.ppLogFile, uniq(touchedFiles));
+  }
+
+  this.callback(null, processed);
 };
 
 export = loader;
